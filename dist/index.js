61f4ac04b1322a784aa01e122fc4c8b9b8af6d23
victoramelinckx <vickx2000@gmail.com>
2024-01-23 11:46:26 -0500

    cambios

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,14 @@
 "use strict";
+const { Command } = require("commander");
 const figlet = require("figlet");
+const program = new Command();
 console.log(figlet.textSync("Buena Basty"));
+program
+    .version("1.0.0")
+    .description("An example CLI for managing a directory")
+    .option("-l, --ls  [value]", "List directory contents")
+    .option("-m, --mkdir <value>", "Create a directory")
+    .option("-t, --touch <value>", "Create a file")
+    .parse(process.argv);
+const options = program.opts();
 //# sourceMappingURL=index.js.map
